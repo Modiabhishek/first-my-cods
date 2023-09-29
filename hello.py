67dc4b303841c8226d73148373f57510f3e64424
Modiabhishek <146158386+Modiabhishek@users.noreply.github.com>
2023-09-29 11:42:22 +0530

    2 day

---
+++ hello.py
@@ -0,0 +1,13 @@
+# print(2+3)
+# print("hello world")
+# print('Hey there')
+# print('hi'*3)
+x=98
+a=8.9
+z="hero"
+print(x,a,z)
+print(type(a))
+print(type(z))
+sum1 = x+a
+mul1 = x*a
+print(sum1,mul1)
---
+++ task1.py
@@ -0,0 +1,7 @@
+n1=input("enter first number: ")
+#input data is string
+n2= input("enter the second number: ")
+# change the data type
+# type casting
+sum = int(n1)+int(n2)
+print(sum)
